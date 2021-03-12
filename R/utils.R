126d6ed285371e1992c0f207953030870f86ba97
Iván Gutiérrez <ivangutierrez1988@gmail.com>
2021-03-12 14:15:22 +0000

    Añade en utils la función get_df_paso_a_paso()

--- R/utils.R
+++ R/utils.R
@@ -1,3 +1,4 @@
+import::from(magrittr, "%>%")
 
 # Stepwise regression para modelos lineales mixtos
 mystepwise <- function(yvar0, xvar0, preserve, max_pval, random, data) {
@@ -17,3 +18,30 @@ mystepwise <- function(yvar0, xvar0, preserve, max_pval, random, data) {
   fmla <- paste0(yvar0, " ~ ", paste(xvar0, collapse = "+"), " + ", random)
   do.call(lme4::lmer, list(formula = as.formula(fmla), data = data))
 }
+
+date_to_sepi <- function(x) {
+  diff <- difftime(
+    strptime(x, format = "%Y-%m-%d"),
+    strptime("2020-02-16", format = "%Y-%m-%d"),
+    units="weeks"
+  )
+  return(as.integer(diff) + 8)
+}
+
+get_df_paso_a_paso <- function() {
+  data <-
+    "https://raw.githubusercontent.com/MinCiencia/Datos-COVID19/master" %>%
+    paste0("/output/producto74/paso_a_paso_std.csv") %>%
+    read.csv() %>%
+    dplyr::rename_with(tolower) %>%
+    dplyr::group_by(fecha, codigo_comuna) %>%
+    dplyr::summarise(paso = min(paso), .groups = "drop") %>%
+    dplyr::mutate(semana = date_to_sepi(fecha)) %>%
+    dplyr::filter(codigo_comuna != 0) %>%
+    dplyr::group_by(semana, codigo_comuna) %>%
+    dplyr::summarise(paso = min(paso), .groups = "drop") %>%
+    dplyr::mutate(paso = factor(paso)) %>%
+    dplyr::arrange(semana, codigo_comuna) %>%
+    tibble::as_tibble()
+}
+# saveRDS(get_df_paso_a_paso(), "data/df_paso_a_paso.rds")
