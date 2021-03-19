a39e7ba784592de7dd5976a5ec87901baf1a76f8
Iván Gutiérrez <ivangutierrez1988@gmail.com>
2021-03-19 18:52:14 +0000

    Add plot_pcr target

--- _targets.R
+++ _targets.R
@@ -44,6 +44,7 @@ list(
   targets::tar_target(b, get_b(fit)),
   targets::tar_target(plot_r, long_boxplot(r_wallinga, "r")),
   targets::tar_target(plot_pvc, long_boxplot(pvc, "pvc")),
+  targets::tar_target(plot_pcr, long_boxplot(pcr, "pcr")),
   targets::tar_target(plot_vacunados1, long_boxplot(vacunados1, "vacunados1")),
   targets::tar_target(plot_vacunados2, long_boxplot(vacunados2, "vacunados2"))
 )
